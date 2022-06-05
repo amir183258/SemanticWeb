1465d1815c1deadee4ee51b8be8bdc3fe9055f41
Amir Hossein <amirhossein183258@gmail.com>
2022-06-05 10:16:16 +0430

    Refactored codes.

--- app/app.js
+++ app/app.js
@@ -1,16 +1,15 @@
 getValueInput = () => {
-    var userInput = document.getElementById("search").value;
+    const userInputBox = document.getElementById("userInput")
+    var userInput = userInputBox.value;
+    userInputBox.value = "";
+
     var inputValue = userInput.charAt(0).toUpperCase() + userInput.slice(1);
 
-    // console.log(inputValue)
     var url = "https://dbpedia.org/sparql";
     var query = [
         `select * { <http://dbpedia.org/resource/${inputValue}> geo:lat ?lat ; geo:long ?long }`
     ].join(" ");
     var temp = url + "?query=" + encodeURIComponent(query) + "&format=xml"
-    console.log(temp);
-
-    // console.log(temp)
 
     const xhttp = new XMLHttpRequest();
     xhttp.onload= function () {
@@ -19,13 +18,31 @@ getValueInput = () => {
         parser = new DOMParser();
         parsedXmlResponseFromDBPedia = parser.parseFromString(xmlResponseFromDBPedia, "text/xml");
 
-        responseLat = parsedXmlResponseFromDBPedia.getElementsByTagName('literal')[0].childNodes[0].nodeValue;
-        responseLong = parsedXmlResponseFromDBPedia.getElementsByTagName('literal')[1].childNodes[0].nodeValue;
-
-        document.getElementById('lat').innerHTML = responseLat;
-        document.getElementById('long').innerHTML = responseLong;
+        try {
+            responseLat = parsedXmlResponseFromDBPedia.getElementsByTagName('literal')[0].childNodes[0].nodeValue;
+            responseLong = parsedXmlResponseFromDBPedia.getElementsByTagName('literal')[1].childNodes[0].nodeValue;
+        }
+        catch (e){
+            inputValue = responseLat = responseLong = false;
+        }
+        finally {
+        displayCoordinates(inputValue, responseLat, responseLong);
+        }
     }
-
     xhttp.open("GET", temp);
     xhttp.send();
+}
+
+function displayCoordinates(city, lat, long) {
+    const resultBox = document.getElementById("resultCoordinates");
+
+    if (city && lat && long) {
+        resultBox.style.display = "block";
+        outputMessage = city + " Coordinates = (" + lat + ", " + long + ")";
+        resultBox.innerHTML = outputMessage;
+    }
+    else {
+        resultBox.innerHTML = "Not in knowledge graph!";
+        resultBox.style.display = "block";
+    }
 }
